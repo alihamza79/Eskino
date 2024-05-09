de6faab3e462d681023fbf408f9cc03f9173f9f3
Hamza <hamzachaudhay79@gmail.com>
2024-05-09 22:32:05 +0500

    Routing for hero section

--- src/approuter.jsx
+++ src/approuter.jsx
@@ -14,30 +14,23 @@ import SettingsChangePassword from "./components/settings/SettingsChangePassword
 //Meeting
 import AddMeeting from "./components/meeting/AddMeeting";
 import EditMeeting from "./components/meeting/EditMeeting";
-//Patients...
-import ParticipentList from "./components/patients/ParticipentList";
-// import AddPatients from "./components/patients/AddPatients";
-import EditParticipent from "./components/patients/EditParticipent";
-import AppoinmentList from "./components/appoinments/AppoinmentList";
-import AddAppoinments from "./components/appoinments/AddAppoinments";
-import EditAppoinments from "./components/appoinments/EditAppoinments";
-//DoctorSchedule
-import ScheduleList from "./components/doctorschedule/ScheduleList";
+import MeetingList from "./components/meeting/MeetingList";
+//Participents...
+import ParticipentList from "./components/participent/ParticipentList";
 
-import AddSchedule from "./components/doctorschedule/AddSchedule";
-import EditSchedule from "./components/doctorschedule/EditSchedule";
-//Departments
-import DepartmentList from "./components/department/DepartmentList";
-import AddDepartment from "./components/department/AddDepartment";
-import EditDepartment from "./components/department/EditDepartment";
-import AdminList from "./components/staff/AdminList";
-import AddAdmin from "./components/staff/Add-Admin";
-import EditAdmin from "./components/staff/EditAdmin";
+import HeroSection from "./components/Landing Page/Hero Section/HeroSection";
+import EditHeroSection from "./components/Landing Page/Hero Section/EditHeroSection";
 
-import ProvidentFund from "./components/accounts/ProvidentFund";
+import EditParticipent from "./components/participent/EditParticipent";
+
+import AddAdmin from "./components/Admin/Add-Admin";
+import AdminList from "./components/Admin/AdminList";
+import EditAdmin from "./components/Admin/EditAdmin";
+
 import ForgotPassword from "./components/pages/login/ForgotPassword";
 import Signup from "./components/pages/login/Signup";
 
+import Admin_Dashboard from "./components/Dashboard/Admin_Dashboard/Admin_Dashboard";
 import BlankPage from "./components/pages/login/BlankPage";
 import ChangePassword from "./components/pages/login/ChangePassword";
 import EditProfile from "./components/pages/login/EditProfile";
@@ -46,7 +39,6 @@ import LockScreen from "./components/pages/login/LockScreen";
 import Profile from "./components/pages/login/Profile";
 import Register from "./components/pages/login/Register";
 import ServerError from "./components/pages/login/ServerError";
-import Admin_Dashboard from "./components/Dashboard/Admin_Dashboard/Admin_Dashboard";
 
 //Accounts
 const Approuter = () => {
@@ -87,18 +79,11 @@ const Approuter = () => {
           <Route path="/participentlist" element={<ParticipentList />} />
           <Route path="/editparticipent" element={<EditParticipent />} />
           {/* Appoinments */}
-          <Route path="/appoinmentlist" element={<AppoinmentList />} />
-          <Route path="/addappoinments" element={<AddAppoinments />} />
-          <Route path="/editappoinments" element={<EditAppoinments />} />
-          {/* DoctorSchedule */}
-          <Route path="/schedulelist" element={<ScheduleList />} />
-          <Route path="/addschedule" element={<AddSchedule />} />
-          <Route path="/editschedule" element={<EditSchedule />} />
-          {/* Department */}
-          <Route path="/departmentlist" element={<DepartmentList />} />
-          <Route path="/add-department" element={<AddDepartment />} />
-          <Route path="/editdepartment" element={<EditDepartment />} />
-          {/* Staff */}
+          <Route path="/herosection" element={<HeroSection />} />
+          <Route path="/editherosection" element={<EditHeroSection />} />
+          {/* <Route path="/editappoinments" element={<EditAppoinments />} /> */}
+
+          {/* Admin */}
           <Route path="/adminlist" element={<AdminList />} />
           <Route path="/addadmin" element={<AddAdmin />} />
           <Route path="/editadmin" element={<EditAdmin />} />
